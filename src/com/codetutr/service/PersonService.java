3fc505f40ea2c73c9d3bed4a443caa928eed8271
srihareendra <srihari.naruto@gmail.com>
2014-10-16 11:11:12 +0530

    Revert "Rest Example"
    This reverts commit ee8b07106ae3fa80ea82896822dd4268699bb1bd.

--- src/com/codetutr/service/PersonService.java
+++
@@ -1,9 +0,0 @@
-package com.codetutr.service;
-
-import com.codetutr.domain.Person;
-
-public interface PersonService {
-	public Person getRandom();
-	public Person getById(Long id);
-	public void save(Person person);
-}
